9ef5e2c1ee328e7a4ccebb74f929793c2949c15c
Ei Ei Khaing <eieikhaing2410@gmail.com>
2019-07-08 13:22:31 +0630

    Custom Line Chart

--- app/src/main/java/com/xan/linechart_mpandroid/MainActivity.java
+++ app/src/main/java/com/xan/linechart_mpandroid/MainActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.listener.OnChartGestureListener;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
@@ -18,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
     private LineChart mChart;
+    float[] prices = {100,200,300,500,300};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +31,22 @@ public class MainActivity extends AppCompatActivity {
         /*mChart.setOnChartGestureListener(MainActivity.this);
         mChart.setOnChartValueSelectedListener(MainActivity.this);*/
         mChart.setDragEnabled(true);
+        mChart.getAxisRight().setEnabled(false);
+        mChart.getDescription().setText("Today price");
+        mChart.getDescription().setPosition(100f,20f);
+
 
         ArrayList<Entry> yValues = new ArrayList<>();
-        yValues.add(new Entry(0,60f));
+        /*for(int i=0 ; i<prices.length ;i++){
+            yValues.add(new Entry(prices[i],"16 Karat"));
+        }*/
+
         yValues.add(new Entry(1,50f));
         yValues.add(new Entry(2,70f));
         yValues.add(new Entry(3,30f));
         yValues.add(new Entry(4,50f));
         yValues.add(new Entry(5,60f));
-        yValues.add(new Entry(6,65f));
+
 
         LineDataSet set1 = new LineDataSet(yValues,"Data set1");
         set1.setColor(Color.RED);
@@ -50,5 +61,25 @@ public class MainActivity extends AppCompatActivity {
         LineData lineData = new LineData(dataSets);
         mChart.setData(lineData);
 
+        String[] values = new String[]{"Feb","Mar","Apr","May","Jun","Jun"};
+        XAxis xAxis = mChart.getXAxis();
+        xAxis.setValueFormatter(new MyxAixValueFormatter(values));
+
+        xAxis.setGranularity(1);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM_INSIDE);
+
+    }
+
+    public class MyxAixValueFormatter implements IAxisValueFormatter
+    {
+        private String[] mValues;
+        public MyxAixValueFormatter(String[] values) {
+            this.mValues = values;
+        }
+
+        @Override
+        public String getFormattedValue(float value, AxisBase axis) {
+            return mValues[(int)value];
+        }
     }
 }
